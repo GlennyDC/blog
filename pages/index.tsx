d1123d60726d05c07d5cea4104390cbdeba7462e
Glenny De Cock <glenny.de.cock@codifly.be>
2020-10-01 21:18:11 +0200

    Add necessary developer setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
-import Head from 'next/head'
-
-export default function Index() {
+export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-    </div>
-  )
+    <main>
+      <h1>
+        Home
+      </h1>
+    </main>
+  );
 }
