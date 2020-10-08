337f4268a6c9a53dd18c385c410ff6c1c443552c
Glenny De Cock <glenny.de.cock@codifly.be>
2020-10-08 19:29:03 +0200

    Rename SVG component

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,9 +1,7 @@
 declare module '*.svg' {
   import * as React from 'react';
 
-  const ReactComponent: React.FC<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
+  const Svg: React.FC<React.SVGProps<SVGSVGElement> & { title?: string }>;
 
-  export default ReactComponent;
+  export default Svg;
 }
